7bb2657436b90c0bdef18f4e3602fc974e3834cd
xamvole-star <xamvole@gmail.com>
2026-09-04 13:06:21 +1000

    Для поиска в гугл настроен user агент для скрытия автоматизации поиска

--- src/test/java/SearchTests.java
+++ src/test/java/SearchTests.java
@@ -1,13 +1,34 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 
 public class SearchTests {
+
+    @BeforeAll
+    static void setUp() {
+        // Настройка User-Agent и скрытие флага автоматизации
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36");
+        options.addArguments("--disable-blink-features=AutomationControlled");
+        options.setExperimentalOption("excludeSwitches", new String[]{"enable-automation"});
+
+        Configuration.browserCapabilities = options;
+        Configuration.timeout = 6000; // Увеличил таймаут, так как Google бывает "тяжелым"
+    }
+
     @Test
     void successfulSearchTest() {
         open("https://www.google.com/");
-        $("[name=q]").setValue("selenide").pressEnter();
-        $("[id=search]").shouldHave(text("https://selenide.org"));
+
+        // В Google поле поиска сейчас часто имеет тег textarea
+        $("textarea[name='q']").setValue("selenide").pressEnter();
+
+        // Проверка результата
+        $("#search").shouldHave(text("https://selenide.org"));
     }
 }
